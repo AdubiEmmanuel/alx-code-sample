627c7234c746576164eb3cef24516eba4b6a842a
Dev-BlessedManuel <blessedmanuelphotos@gmail.com>
2024-01-28 16:59:10 +0100

    new function, sub, mult added

--- lesson1/functions.py
+++ lesson1/functions.py
@@ -1,7 +1,12 @@
 def add(a, b):
     return a + b  
+def sub(a, b):
+    return a - b  
+def mult(a, b):
+    return a * b  
 
 
 
 print(add(44,66))
-print(add(4,6))
+print(sub(4,6))
+print(mult(4,6))
